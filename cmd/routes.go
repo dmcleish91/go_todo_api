8a3bad9ae7988e988487561f9312a49347b87fd5
Dwight Mcleish Jr <dmcleishjr@gmail.com>
2025-06-02 01:12:51 -0400

    Revert "fix: update CORS configuration to allow all origins"
    This reverts commit 31a71debbfb92319727ebacf93009214edddfd73.

--- cmd/routes.go
+++ cmd/routes.go
@@ -28,7 +28,7 @@ func (app *application) Routes() *echo.Echo {
 	}))
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"http://localhost:5173", "https://yata-one.vercel.app"},
 		AllowMethods:     []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		AllowCredentials: true,
